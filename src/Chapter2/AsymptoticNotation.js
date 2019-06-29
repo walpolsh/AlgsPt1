ae6144d0e019b922980c45ac61db4700ccc39c0b
Paul Walsh <twentytwentyk@live.com>
2019-06-29 11:58:22 -0400

    searchTwo duplicates

--- src/Chapter2/AsymptoticNotation.js
+++ src/Chapter2/AsymptoticNotation.js
@@ -2,14 +2,29 @@ import React from "react";
 function search(arr, val) {
   return arr.includes(val) ? true : false;
 }
+function searchTwo(arr1, arr2, val) {
+  return arr1.includes(val) ? true : arr2.includes(val) ? true : false;
+}
+function duplicates(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr1.length; j++) {
+      if (arr1[i] === arr2[j + 1]) {
+        return `true, there is a duplicate: ${arr1[i]} === ${arr2[j + 1]}`;
+      }
+    }
+  }
+  return false;
+}
 let arr = [12, 3, 5, 22, 4];
+let arr2 = [15, 26, 37, 3, 48, 9];
 
 export function AsymptoticNotation() {
   return (
     <div>
       <div>{search(arr, 2).toString()}</div>
       <div>{search(arr, 3).toString()}</div>
-      <div>{}</div>
+      <div>{searchTwo(arr, arr2, 48).toString()}</div>
+      <div>{duplicates(arr, arr2)}</div>
       <div>{}</div>
       <div>{}</div>
       <div>{}</div>
